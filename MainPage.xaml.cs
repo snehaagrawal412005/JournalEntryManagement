f11c47c45795b4b8100b2b696a709ad645e4e51f
snehaagrawal412005 <snehaagrawal412005@gmail.com>
2025-12-20 23:07:09 +0545

    Milestone 1: Initial project setup with .gitignore and MAUI structured

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JournalEntryManagement
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
