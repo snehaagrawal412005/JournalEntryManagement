d4f2e3ec9cc5af50a617539f8ba209b088d5019e
snehaagrawal412005 <snehaagrawal412005@gmail.com>
2025-12-20 23:38:04 +0545

    Milestone 1: Implemended data model, and Journal UI and the  mood tracking

---
+++ Models/JournalEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalEntryManagement.Models
+{
+    public class JournalEntry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Content { get; set; } = "";
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string PrimaryMood { get; set; } = "Neutral";
+        public string Tags { get; set; } = "";
+    }
+}
